a8691d215a2ebc02fea35a5af3f350d8f830dd12
kkaizer11 <kaizerin.m@gmail.com>
2023-08-21 23:08:51 -0300

    nem sei

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
 
 def api_home(request, *arg, **kwarg):
+    body = request.body
+    print(body)
     return JsonResponse({"message": "Json answer, we come in peace"})
